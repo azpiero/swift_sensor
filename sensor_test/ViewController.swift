b6d36748518e57f4741d8edc08995797996ca6db
azpiero <joek0624@gmail.com>
2018-07-09 07:36:32 +0900

    Initial Commit

---
+++ sensor_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sensor_test
+//
+//  Created by kishimoto on 2018/07/09.
+//  Copyright © 2018年 kishimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
